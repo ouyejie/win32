c79946064cef15d5f5ccf2a99096b32921b86508
ouyejie <1175016496@qq.com>
2016-12-30 18:41:52 +0800

    add screenshot project

---
+++ screenshot/screenshot/src/main.cpp
@@ -0,0 +1,192 @@
+// screenshot.cpp : 定义应用程序的入口点。
+//
+
+
+#include "stdafx.h"
+#include <stdio.h>
+
+#pragma once
+#include "resource.h"
+
+#define MAX_LOADSTRING 100
+
+// 全局变量: 
+HINSTANCE hInst;								// 当前实例
+TCHAR szTitle[MAX_LOADSTRING];					// 标题栏文本
+TCHAR szWindowClass[MAX_LOADSTRING];			// 主窗口类名
+
+int gCxScreen;
+int gCyScreen;
+
+HDC gWindowDC;
+HDC gMemDC;
+
+// 此代码模块中包含的函数的前向声明: 
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPTSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	gCxScreen = ::GetSystemMetrics(SM_CXVIRTUALSCREEN);	//获取设备宽度
+	gCyScreen = ::GetSystemMetrics(SM_CYVIRTUALSCREEN);	//获取设备高度
+
+	gWindowDC = GetDC(::GetDesktopWindow());
+	RECT rect;
+	::GetClientRect(::GetDesktopWindow(), &rect);
+	gMemDC = ::CreateCompatibleDC(gWindowDC);
+	HBITMAP bitMap = ::CreateCompatibleBitmap(gWindowDC, rect.right, rect.bottom);
+	HBITMAP oldMap = (HBITMAP)::SelectObject(gMemDC, bitMap);
+	::BitBlt(gMemDC, 0, 0, rect.right, rect.bottom, gWindowDC, 0, 0, SRCCOPY);
+
+ 	// TODO:  在此放置代码。
+	MSG msg;
+	HACCEL hAccelTable;
+
+	// 初始化全局字符串
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_SCREENSHOT, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// 执行应用程序初始化: 
+	if (!InitInstance (hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_SCREENSHOT));
+
+	// 主消息循环: 
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int) msg.wParam;
+}
+
+//
+//  函数:  MyRegisterClass()
+//
+//  目的:  注册窗口类。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SCREENSHOT));
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= NULL;//MAKEINTRESOURCE(IDC_SCREENSHOT);
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   函数:  InitInstance(HINSTANCE, int)
+//
+//   目的:  保存实例句柄并创建主窗口
+//
+//   注释: 
+//
+//        在此函数中，我们在全局变量中保存实例句柄并
+//        创建和显示主程序窗口。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd;
+
+   hInst = hInstance; // 将实例句柄存储在全局变量中
+
+   hWnd = CreateWindow( szWindowClass, 
+						szTitle,
+						WS_POPUP,//WS_OVERLAPPEDWINDOW,
+						0,
+						0,
+						gCxScreen,
+						gCyScreen,
+						NULL, 
+						NULL, 
+						hInstance, 
+						NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }   
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  函数:  WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目的:    处理主窗口的消息。
+//
+//  WM_COMMAND	- 处理应用程序菜单
+//  WM_PAINT	- 绘制主窗口
+//  WM_DESTROY	- 发送退出消息并返回
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+		return 0;
+		break;
+	case WM_SIZE:
+		return 0;	
+	case WM_MOUSEMOVE:
+		break;
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			break;
+		}
+		break;
+	case WM_PAINT:
+	{
+		hdc = BeginPaint(hWnd, &ps);
+
+		BitBlt(hdc, 0, 0, gCxScreen, gCyScreen, gMemDC, 0, 0, SRCCOPY);
+
+		EndPaint(hWnd, &ps);
+		break;
+	}
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
